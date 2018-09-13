d8555e45d2b4031110ff40c422bd69eb621205f3
aegooby <sunrize187@gmail.com>
2018-09-13 21:22:39 +0300

    Build fix

--- build/tsp.build/Debug/ZERO_CHECK.build/Script-46ED9576F69E4A87891E01D0.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/admin/Documents/0x0/Repositories/tsp/build -f /Users/admin/Documents/0x0/Repositories/tsp/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
